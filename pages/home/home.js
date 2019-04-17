473f8a1bf0bb341048f1e7dce7aa08004a857f93
wanming <zhouhuixian@tiaohuo.com>
2019-04-17 16:16:52 +0800

    首页显示折扣列表

--- pages/home/home.js
+++ pages/home/home.js
@@ -22,8 +22,6 @@ Page(Object.assign({}, swiperAutoHeight, {
     autoplay: true,
     interval: 4000,
     duration: 1000,
-    limitSize: 1,
-    limitPage: 1,
     timer: null
   },
 
@@ -96,107 +94,9 @@ Page(Object.assign({}, swiperAutoHeight, {
       }
     })
   },
-  nextLimit() {
-    var that = this
-    clearInterval(that.timer);
-    if (this.data.limitPage == this.data.limitCount) {
-      this.setData({
-        limitPage: 1
-      })
-    } else {
-      this.setData({
-        limitPage: ++that.data.limitPage
-      })
-    }
-    new Product(res => {
-      var len = res.data.data.length
-      if (len == 0) {
-        that.setData({
-          limitLength: true,
-          limitCount: res.data.count
-        })
-      } else {
-        that.setData({
-          limitLength: false,
-          limitCount: res.data.count
-        })
-      }
-
-      function time1() {
-        var limitsell = res.data.data
-        for (var i = 0; i < limitsell.length; i++) {
-          // 活动是否已经开始
-          var totalSecond = limitsell[i].beginDate / 1000 - Date.parse(new Date()) / 1000;
-          // 活动是否已经结束
-          var endSecond = limitsell[i].endDate / 1000 - Date.parse(new Date()) / 1000;
-          // 秒数
-
-          if (totalSecond < 0 && endSecond > 0) {
-            var second = endSecond;
-          } else {
-            var second = totalSecond;
-          }
-
-          // 天数位
-          var day = Math.floor(second / 3600 / 24);
-          var dayStr = day.toString();
-          if (dayStr.length == 1) dayStr = '0' + dayStr;
-
-          // 小时位
-          var hr = Math.floor((second - day * 3600 * 24) / 3600);
-          var hrStr = hr.toString();
-          if (hrStr.length == 1) hrStr = '0' + hrStr;
-
-          // 分钟位
-          var min = Math.floor((second - day * 3600 * 24 - hr * 3600) / 60);
-          var minStr = min.toString();
-          if (minStr.length == 1) minStr = '0' + minStr;
-
-          // 秒位
-          var sec = second - day * 3600 * 24 - hr * 3600 - min * 60;
-          var secStr = sec.toString();
-          if (secStr.length == 1) secStr = '0' + secStr;
-          totalSecond--;
-          if (totalSecond < 0 && endSecond > 0) {
-            limitsell[i].txt = '马上秒'
-            limitsell[i].countDownDay = dayStr
-            limitsell[i].countDownHour = hrStr
-            limitsell[i].countDownMinute = minStr
-            limitsell[i].countDownSecond = secStr
-            that.setData({
-              limitsell: limitsell
-            });
-          } else if (totalSecond > 0) {
-            limitsell[i].txt = '即将开秒'
-            limitsell[i].countDownDay = dayStr
-            limitsell[i].countDownHour = hrStr
-            limitsell[i].countDownMinute = minStr
-            limitsell[i].countDownSecond = secStr
-
-            that.setData({
-              limitsell: limitsell
-            });
-          } else if (totalSecond < 0 && endSecond < 0) {
-            clearInterval(that.timer);
-            limitsell[i].txt = '去看看'
-            limitsell[i].countDownDay = '00'
-            limitsell[i].countDownHour = '00'
-            limitsell[i].countDownMinute = '00'
-            limitsell[i].countDownSecond = '00'
-            that.setData({
-              limitsell: limitsell
-            });
-          }
-        }
-        that.setData({
-          limitsell: limitsell,
-        })
-      }
-      time1();
-      that.timer = setInterval(time1, 1000);
-    }).indexPromotion({
-      size: 1,
-      page: that.data.limitPage
+  nextLimit(){
+    wx.navigateTo({
+      url: 'seckill/index',
     })
   },
   getData: function(options) {
@@ -215,13 +115,11 @@ Page(Object.assign({}, swiperAutoHeight, {
       var len = res.data.data.length
       if (len == 0) {
         that.setData({
-          limitLength: true,
-          limitCount: res.data.count
+          limitLength: true
         })
       } else {
         that.setData({
-          limitLength: false,
-          limitCount: res.data.count
+          limitLength: false
         })
       }
 
@@ -301,16 +199,12 @@ Page(Object.assign({}, swiperAutoHeight, {
       size: 1,
       page: 1
     })
-
     new Product(function(data) {
       that.setData({
         categoryList: data.data.categoryList,
         banner: data.data.banner,
         hotGoodsList: data.data.hotGoodsList
       })
-
-
-
       // console.log(data)
     }).list()
   },
